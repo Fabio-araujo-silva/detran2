d1a908e216a3c998de845bf7c591caedce224417
fabio-araujo-silva <fabio.kaue.araujo@gmail.com>
2023-12-10 17:55:15 -0300

    gerar mais de um carro

--- jogo.py
+++ jogo.py
@@ -18,6 +18,9 @@ sem2 = pygame.image.load("semaforo.png").convert_alpha()
 sem3 = pygame.image.load("semaforo.png").convert_alpha()
 sem4 = pygame.image.load("semaforo.png").convert_alpha()
 carro = pygame.image.load("carro.png").convert_alpha()
+carro2 = pygame.transform.rotate(carro, 180)
+carro3 = pygame.transform.rotate(carro, -90)
+carro4 = pygame.transform.rotate(carro, 90)
 
 # Redimensionar os sprites
 novo_largura_sem, novo_altura_sem = 100, 100
@@ -27,16 +30,25 @@ sem2 = pygame.transform.scale(sem2, (novo_largura_sem, novo_altura_sem))
 sem3 = pygame.transform.scale(sem3, (novo_largura_sem, novo_altura_sem))
 sem4 = pygame.transform.scale(sem4, (novo_largura_sem, novo_altura_sem))
 carro = pygame.transform.scale(carro, (novo_largura_carro, novo_altura_carro))
+carro2 = pygame.transform.scale(carro2, (novo_largura_carro, novo_altura_carro))
+carro3 = pygame.transform.scale(carro3, (novo_altura_carro,novo_largura_carro))
+carro4 = pygame.transform.scale(carro4, (novo_altura_carro,novo_largura_carro))
 
 # Posição inicial do carro
 carro_inicial_pos = (290, 0)
+carro2_inicial_pos = (350, 700)
+carro3_inicial_pos = (700, 300)
+carro4_inicial_pos = (0, 370)
 
 # Lista de sprites e suas posições
-sprites = [(sem1, (200, 150)), (sem2, (100, 450)), (sem3, (500, 150)), (sem4, (400, 450)), (carro, carro_inicial_pos)]
+sprites = [(sem1, (200, 150)), (sem2, (100, 450)), (sem3, (500, 150)), (sem4, (400, 450)), (carro, carro_inicial_pos), (carro2, carro2_inicial_pos), (carro3, carro3_inicial_pos), (carro4, carro4_inicial_pos)]
 
 # Controle de tempo para gerar novos carros
-tempo_para_novo_carro = 3000  # Tempo em milissegundos (1 segundo = 1000 milissegundos)
+tempo_para_novo_carro = 5000
 tempo_acumulado = 0
+
+# Velocidade dos carros
+velocidade_carro = 0.05
 
 # Loop principal
 while True:
@@ -48,7 +60,16 @@ while True:
     # Atualizar a posição dos carros existentes
     for i, (sprite, pos) in enumerate(sprites):
         if sprite == carro:  # Ajustar apenas a posição do carro
-            nova_posicao = (pos[0], pos[1] + 1)  # Movendo o carro para baixo
+            nova_posicao = (pos[0], pos[1] + velocidade_carro)  # Movendo o carro para baixo
+            sprites[i] = (sprite, nova_posicao)
+        elif sprite == carro2:
+            nova_posicao = (pos[0], pos[1] - velocidade_carro)  # Movendo o carro para cima
+            sprites[i] = (sprite, nova_posicao)
+        elif sprite == carro3:
+            nova_posicao = (pos[0] - velocidade_carro, pos[1])  # Movendo o carro para a esquerda
+            sprites[i] = (sprite, nova_posicao)
+        elif sprite == carro4:
+            nova_posicao = (pos[0] + velocidade_carro, pos[1])  # Movendo o carro para a direita
             sprites[i] = (sprite, nova_posicao)
 
     # Controle de tempo para gerar novos carros
@@ -57,7 +78,15 @@ while True:
 
     # Gerar um novo carro a cada segundo
     if tempo_decorrido > tempo_para_novo_carro:
-        novo_sprite, nova_posicao = carro, carro_inicial_pos
+        # Alternar entre os carros
+        if len(sprites) % 4 == 0:
+            novo_sprite, nova_posicao = carro, carro_inicial_pos
+        elif len(sprites) % 4 == 1:
+            novo_sprite, nova_posicao = carro2, carro2_inicial_pos
+        elif len(sprites) % 4 == 2:
+            novo_sprite, nova_posicao = carro3, carro3_inicial_pos
+        else:
+            novo_sprite, nova_posicao = carro4, carro4_inicial_pos
         sprites.append((novo_sprite, nova_posicao))
         tempo_acumulado = tempo_passado
 
